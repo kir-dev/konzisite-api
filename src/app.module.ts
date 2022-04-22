3bf7dd8ecb4c9d6d07a31b296297cdb43ece3989
Tenyitamas <tenyi.tamas28@gmail.com>
2022-04-22 13:21:59 +0200

    Added User and Group Entity without Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, GroupsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,7 @@
 export class User {
-  name: string;
+  firstName: string;
+
+  lastName: string;
 
   email: string;
 
