f0e1ddfa5de76f35a4508ee2e3701c657132ed6d
tomitheninja <sudi.tamas@simonyi.bme.hu>
2022-08-12 00:27:19 +0200

    add auth.me handler to check authorization

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { AuthGuard } from '@nestjs/passport'
 import { User } from '@prisma/client'
 import { CurrentUser } from 'src/current-user.decorator'
 import { AuthService } from './auth.service'
+import { JwtAuthGuard } from './jwt-auth.guard'
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +23,11 @@ export class AuthController {
   async oauthRedirect(@CurrentUser() user: User) {
     const { jwt } = this.authService.login(user)
     return { url: `${process.env.FRONTEND_AUTHORIZED_URL}?jwt=${jwt}` }
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  me(@CurrentUser() user: User) {
+    return user
   }
 }
