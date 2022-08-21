c0313bb6c5a3f8a49c8137acabfddd6a7be8fbc1
Tenyitamas <tenyi.tamas28@gmail.com>
2022-08-21 13:36:59 +0200

    Implement the Participation & Presentation service

--- src/consultations/presentation.service.ts
+++ src/consultations/presentation.service.ts
@@ -1,7 +1,49 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
 export class PresentationService {
   constructor(private prisma: PrismaService) {}
+  create(data: Prisma.PresentationUncheckedCreateInput) {
+    return this.prisma.presentation.create({ data })
+  }
+  async findAllByUserId(userId: number) {
+    const presentations = await this.prisma.presentation.findMany({
+      where: {
+        userId,
+      },
+    })
+
+    return presentations
+  }
+
+  async findAllByConsultationId(consultationId: number) {
+    const presentations = await this.prisma.presentation.findMany({
+      where: {
+        consultationId,
+      },
+    })
+
+    return presentations
+  }
+
+  async findOne(consultationId: number, userId: number) {
+    const presentation = await this.prisma.presentation.findUnique({
+      where: {
+        consultationId_userId: {
+          consultationId: consultationId,
+          userId: userId,
+        },
+      },
+    })
+
+    return presentation
+  }
+
+  remove(id: number) {
+    return this.prisma.presentation.delete({
+      where: { id },
+    })
+  }
 }
