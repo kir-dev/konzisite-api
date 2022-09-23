06045b9cb19d5b49c344886f87a511a136aeb8a3
Sámuel Fekete <feketesamu@gmail.com>
2022-09-23 23:20:48 +0200

    min(1) validator to manyuniqueusers

--- src/users/dto/ManyUniqueUsers.dto.ts
+++ src/users/dto/ManyUniqueUsers.dto.ts
@@ -1,6 +1,7 @@
-import { IsInt } from 'class-validator'
+import { IsInt, Min } from 'class-validator'
 
 export class ManyUniqueUsersDto {
   @IsInt({ each: true })
+  @Min(1, { each: true })
   userIds: number[]
 }
