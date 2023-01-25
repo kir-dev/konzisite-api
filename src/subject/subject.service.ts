978ebf185c7fa890ffc8b54989a0d56fb1dd354f
Sámuel Fekete <feketesamu@gmail.com>
2023-01-25 13:11:55 +0100

    Subject import (#91)
    * subject import
    
    * postman update
    
    * fixes

--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,30 +1,35 @@
 import { Injectable } from '@nestjs/common'
-import { Subject } from '@prisma/client'
+import { CreateManyResponse } from 'src/utils/CreateManyResponse.dto'
 import { PrismaService } from '../prisma/prisma.service'
 import { CreateSubjectDto } from './dto/CreateSubject.dto'
+import { SubjectEntity } from './dto/SubjectEntity.dto'
 import { UpdateSubjectDto } from './dto/UpdateSubject.dto'
 
 @Injectable()
 export class SubjectService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: CreateSubjectDto): Promise<Subject> {
+  async create(data: CreateSubjectDto): Promise<SubjectEntity> {
     return this.prisma.subject.create({ data })
   }
 
-  async findAll(): Promise<Subject[]> {
+  async createMany(data: CreateSubjectDto[]): Promise<CreateManyResponse> {
+    return this.prisma.subject.createMany({ data })
+  }
+
+  async findAll(): Promise<SubjectEntity[]> {
     return this.prisma.subject.findMany()
   }
 
-  async findOne(id: number): Promise<Subject> {
+  async findOne(id: number): Promise<SubjectEntity> {
     return this.prisma.subject.findUnique({ where: { id: id } })
   }
 
-  async update(id: number, data: UpdateSubjectDto): Promise<Subject> {
+  async update(id: number, data: UpdateSubjectDto): Promise<SubjectEntity> {
     return this.prisma.subject.update({ data, where: { id: id } })
   }
 
-  async remove(id: number): Promise<Subject> {
+  async remove(id: number): Promise<SubjectEntity> {
     return this.prisma.subject.delete({ where: { id: id } })
   }
 }
