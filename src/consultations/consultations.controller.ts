4c67679c384e1bf39012a2bd62ec6f1d99a81a91
Frigyes309 <frigyes.smuk@gmail.com>
2023-03-16 17:52:18 +0100

    Missing error handling but seems fine

--- src/consultations/consultations.controller.ts
+++ src/consultations/consultations.controller.ts
@@ -36,9 +36,9 @@ import {
 import { RequiredPermission } from 'src/auth/decorator/requiredPermission'
 import { RequestsService } from 'src/requests/requests.service'
 import { UserEntity } from 'src/users/dto/UserEntity.dto'
-import { ApiController } from 'src/utils/apiController.decorator'
 import { FileExtensionValidator } from 'src/utils/FileExtensionValidator'
 import { FileMaxSizeValidator } from 'src/utils/FileMaxSizeValidator'
+import { ApiController } from 'src/utils/apiController.decorator'
 import { AlertService } from './alert.service'
 import { ConsultationsService } from './consultations.service'
 import { ConsultationDetailsDto } from './dto/ConsultationDetails.dto'
@@ -448,5 +448,78 @@ export class ConsultationsController {
       'Ehhez a konzultációhoz nincs feltöltve fájl!',
       HttpStatus.NOT_FOUND,
     )
+  }
+
+  @JwtAuth()
+  @RequiredPermission(Permissions.JoinConsultation)
+  @Get(':id/ics')
+  async getCalendar(
+    @Param('id', ParseIntPipe) id: number,
+    @CurrentUser() user: UserEntity,
+    @Res() res: Response,
+  ) {
+    const consultation = await this.consultationsService.findOne(id, user)
+
+    const startString =
+      consultation.startDate.getFullYear().toString() +
+      ('0' + consultation.startDate.getMonth().toString()).slice(-2) +
+      ('0' + consultation.startDate.getDate().toString()).slice(-2) +
+      `T` +
+      ('0' + consultation.startDate.getHours().toString()).slice(-2) +
+      ('0' + consultation.startDate.getMinutes().toString()).slice(-2) +
+      ('0' + consultation.startDate.getSeconds().toString()).slice(-2)
+    const endString =
+      consultation.endDate.getFullYear().toString() +
+      ('0' + consultation.endDate.getMonth().toString()).slice(-2) +
+      ('0' + consultation.endDate.getDate().toString()).slice(-2) +
+      `T` +
+      ('0' + consultation.endDate.getHours().toString()).slice(-2) +
+      ('0' + consultation.endDate.getMinutes().toString()).slice(-2) +
+      ('0' + consultation.endDate.getSeconds().toString()).slice(-2)
+
+    const eventText = `BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:REQUEST
+BEGIN:VTIMEZONE
+TZID:Europe/Budapest
+X-LIC-LOCATION:Europe/Budapest
+BEGIN:DAYLIGHT
+TZOFFSETFROM:+0100
+TZOFFSETTO:+0200
+TZNAME:CEST
+RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU
+END:DAYLIGHT
+BEGIN:STANDARD
+TZOFFSETFROM:+0200
+TZOFFSETTO:+0100
+TZNAME:CET
+DTSTART:19701025T030000
+RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU
+END:STANDARD
+END:VTIMEZONE
+BEGIN:VEVENT
+DTSTART;TZID=Europe/Budapest:${startString}
+DTEND;TZID=Europe/Budapest:${endString}
+X-MICROSOFT-CDO-OWNERAPPTID:-1911733949
+DESCRIPTION: ${consultation.descMarkdown.toString()}
+LOCATION:${consultation.location.toString()}
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:${consultation.name.toString()}
+TRANSP:OPAQUE
+END:VEVENT
+END:VCALENDAR`
+
+    if (!consultation.archived) {
+      res.set({
+        'Content-Disposition':
+          'attachment; filename="konzultacio_' + consultation.id + '.ics"',
+        'Content-type': 'text/plain',
+      })
+      res.send(eventText) //TODO ?? Error handling
+    }
+    throw new HttpException('Az esemény archiválva lett', HttpStatus.NOT_FOUND)
   }
 }
